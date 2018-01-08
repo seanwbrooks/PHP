c15e7b7c0a9c33c0b09369fc8c768080f6be26b4
Sean <swbrooks87@gmail.com>
2018-01-08 11:54:59 -0500

    WIP Checking solution

--- dealer_science.md
+++ dealer_science.md
@@ -1,23 +1,19 @@
-DealerScience Code Assessment
-Given any random string &quot;dgfddsssdsfdffdfdfadfhheedsfdfcdfdxffdh&quot; and N as a number of unique
-characters an input parameter, find the longest substring made entirely of N unique characters. These
-characters may be repeated an unlimited number of times, but must be adjacent to each other in the
-string without any other characters between them. Output the longest substring and its starting position
-in the given string. If n=2 output &quot;fdffdfdf&quot; starts at position 10.
+# DealerScience Code Assessment
+⋅⋅⋅Given any random string &quot;dgfddsssdsfdffdfdfadfhheedsfdfcdfdxffdh&quot; and N as a number of unique characters an input parameter, find the longest substring made entirely of N unique characters. These characters may be repeated an unlimited number of times, but must be adjacent to each other in the string without any other characters between them. Output the longest substring and its starting position in the given string. If n=2 output &quot;fdffdfdf&quot; starts at position 10.
 
-Input: 
+### Input: 
 1. string of characters (String)
 2. number of unique characters (Integer)
 
-Output:
+### Output:
 Longest string - starting position
 
-Example:
+#### Example:
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 1. dgfddsssdsfdffdfdfadfhheedsfdfcdfdxffdh
 2. N = 2
 
-Substrings:
+#### Substrings:
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 dg (length 2)
 gf (length 2)
--- dealer_science.php
+++ dealer_science.php
@@ -1,7 +1,7 @@
 <?php
 
 // The following is my practice solution, which doesn't handle N > 2
-function findMaxSubstring($string, $N) {
+function findMaxSubstringPractice($string, $N) {
   // Initial variables
   $position = 0;
   $string = str_split($string);
@@ -24,7 +24,7 @@ function findMaxSubstring($string, $N) {
     }
   }
 
-  return implode($max_substring['substring']) . ' (length ' . $max_substring['length'] . ') at position ' . $max_substring['position'];
+  return implode($max_substring['substring']) . ' (length ' . $max_substring['length'] . ') at position ' . $max_substring['position'] . "\n";
 }
 
 // sets unique characters
@@ -37,12 +37,13 @@ function setUniqueChars($string, $position, $N) {
 }
 
 // Display output using example input
-echo findMaxSubstring("dgfddssssdsfdffdfdfadfhheedsfdfcdfdxffdh", 2);
+echo findMaxSubstringPractice("dgfddssssdsfdffdfdfadfhheedsfdfcdfdxffdh", 2);
+
+
 
 // The best solution with O(n) implementation
 
-
-$MAX_CHARS = 26;
+$max_chars = 26;
 
 // This function calculates number of unique characters
 // using an associative array count[]. Returns true if
@@ -50,27 +51,76 @@ $MAX_CHARS = 26;
 // false.
 function isValid($count, $N) {
   $val = 0;
-  for ($i = 0; $i < $MAX_CHARS; $i++) {
+  for ($i = 0; $i < $GLOBALS['max_chars']; $i++) {
     if ($count[$i] > 0) {
       $val++;
     }
   }
-  // Returns true if k is greater than or equal to val
-  return ($N >= $val)
+  // Returns true if $N is greater than or equal to $val
+  return ($N >= $val);
 }
 
 // Finds the maximum substring with exactly N unique chars
 function findMaxSubstring($string, $N) {
-  $u = 0; // number of unique characters
-  $n = count($string);
+  $uniqChars = 0; // number of unique characters
+  $string_len = count($string);
 
   // Associative array to store the count
-  $count = [0] * $MAX_CHARS;
+  $count = array_fill(0, $GLOBALS['max_chars'], 0);
+
+  var_dump(array('count_array' => $count, 'max_chars' => $GLOBALS['max_chars']));
 
   // Traverse the string, fills the associative array
   // count[] and count number of unique characters
-  for ($i = 0; $i < $n; $i++) {
-    if ($count)
-    // Pick up here...
+  for ($i = 0; $i < $string_len; $i++) {
+    if ($count[$string[$i] - 'a'] == 0) {
+      $u++;
+      $count[$string[$i] - 'a']++;
+    }
   }
+
+  // If there are not enough unique characters, show an error message
+  if ($u < $k) {
+    return "Not enough unique characters";
+  }
+
+  // Otherwise take a window with first element in it.
+  // Start and end variables.
+  $cur_start = $cur_end = 0;
+
+  // Also initialize values for result longest window
+  $max_window_size = 1;
+  $max_window_start = 0;
+
+  // Initialize associative array count[] with zero
+  $count = array_fill(0, $GLOBALS['max_chars'], 0);
+
+  $count[$string[0] - 'a']++; // put the first character
+
+  // Start from the second character and add characters in window according
+  // to above explanation.
+  for ($i = 1; $i < $string_len; $i++) {
+    // Add the character '$string[$i]' to current window
+    $count[$string[$i] - 'a']++;
+    $cur_end++;
+
+    // If there are more than N unique characters in current window, remove
+    // from left side
+    while (!isValid($count, $N)) {
+      $count[$string[$cur_start] - 'a']--;
+      $cur_start++;
+    }
+
+    // Update the max window size if required
+    if ($cur_end - $cur_start + 1 > $max_window_size) {
+      $max_window_size = $cur_end - $cur_start + 1;
+      $max_window_start = $cur_start;
+    }
+  }
+
+  return "Max substring is: " . $string.substr($max_window_start, $max_window_size) .
+    " with length " . $max_window_size;
 }
+
+// Call function for results
+echo findMaxSubstring("aabacbebebe", 3);
